f1ae87af0a085a93e541d5fce1144c78a663caec
Jay Honnold <jayhonnold@gmail.com>
2020-07-30 15:29:36 -0500

    Config cleanup

--- src/main/kotlin/me/honnold/ladderhero/config/FlywayConfig.kt
+++ src/main/kotlin/me/honnold/ladderhero/config/FlywayConfig.kt
@@ -6,9 +6,16 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-open class FlywayConfig(private val flywayProperties: FlywayProperties) {
+open class FlywayConfig {
+    @Bean("flywayProperties")
+    open fun getFlywayProperties(): FlywayProperties {
+        return FlywayProperties()
+    }
+
     @Bean(initMethod = "migrate")
     open fun flyway(): Flyway? {
+        val flywayProperties = getFlywayProperties()
+
         return Flyway(
             Flyway.configure().baselineOnMigrate(true)
                 .dataSource(flywayProperties.url, flywayProperties.user, flywayProperties.password)
--- src/main/kotlin/me/honnold/ladderhero/config/SecurityConfig.kt
+++ src/main/kotlin/me/honnold/ladderhero/config/SecurityConfig.kt
@@ -3,15 +3,10 @@ package me.honnold.ladderhero.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
 import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.server.SecurityWebFilterChain
 
 @Configuration
