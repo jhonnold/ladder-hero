7c114382e810ffd599b54d81e0f76e8c12ddedaa
Jay Honnold <jayhonnold@gmail.com>
2020-07-30 07:34:42 -0500

    Basic authentication added

---
+++ src/main/kotlin/me/honnold/ladderhero/config/SecurityConfig.kt
@@ -0,0 +1,46 @@
+package me.honnold.ladderhero.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+@EnableWebFluxSecurity
+open class SecurityConfig {
+    @Value("\${admin.user}")
+    lateinit var adminUser: String
+
+    @Value("\${admin.password}")
+    lateinit var adminPassword: String
+
+    @Bean
+    open fun securityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http
+            .csrf().disable()
+            .authorizeExchange().anyExchange().authenticated()
+            .and().httpBasic()
+            .and().formLogin().disable()
+            .build()
+    }
+
+    @Bean
+    open fun userDetailsService(): MapReactiveUserDetailsService {
+        val admin = User.builder()
+            .username(adminUser)
+            .password(adminPassword)
+            .roles("ADMIN")
+            .build()
+
+        return MapReactiveUserDetailsService(admin)
+    }
+}
