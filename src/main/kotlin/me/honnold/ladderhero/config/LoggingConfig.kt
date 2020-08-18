276bb157eb724510b8a00664d78ad7734d4aa032
Jay Honnold <jayhonnold@gmail.com>
2020-08-18 17:49:34 -0500

    Log requests

---
+++ src/main/kotlin/me/honnold/ladderhero/config/LoggingConfig.kt
@@ -0,0 +1,28 @@
+package me.honnold.ladderhero.config
+
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.server.WebFilter
+import java.util.*
+
+@Configuration
+open class LoggingConfig {
+    companion object {
+        private val logger = LoggerFactory.getLogger(LoggingConfig::class.java)
+    }
+
+    @Bean
+    open fun loggingFilter(): WebFilter {
+        return WebFilter { exchange, chain ->
+            val start = Date()
+            val request = exchange.request
+            val result = chain.filter(exchange)
+
+            result.doOnTerminate {
+                val time = Date().time - start.time
+                logger.info("${exchange.response.rawStatusCode} ${request.method} ${request.path.pathWithinApplication()} [${request.queryParams}] ${time}ms")
+            }
+        }
+    }
+}
